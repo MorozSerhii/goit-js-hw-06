28de1c2641d18abcbaf040fad5511ad9718ed93a
Serhii <ecprap@gmail.com>
2023-01-23 13:43:33 +0200

    end

--- js/task-10.js
+++ js/task-10.js
@@ -5,18 +5,25 @@ function getRandomHexColor() {
 const inputEl = document.querySelector('#controls input');
 const create = document.querySelector('[data-create]');
 const destroy = document.querySelector('[data-destroy]');
-const boxes = document.getElementById('boxes');
+const boxes = document.querySelector('#boxes');
+
+function divSize() {
+  let size = 30;
+  return size;
+}
 
 const createBoxes = function (amount) {
   const arrayDivs = [];
-  let size = 30;
+  let size = divSize();
   for (let i = 0; i < amount; i++) {
     const item = document.createElement('div');
     item.style.backgroundColor = getRandomHexColor();
     item.style.width = `${size}px`;
     item.style.height = `${size}px`;
-    size += 10;
+    item.classList.add('animation');
+
     arrayDivs.push(item);
+    size += 10;
   }
   boxes.append(...arrayDivs);
 };
@@ -27,8 +34,8 @@ create.addEventListener('click', () => {
   }
   createBoxes(inputEl.value);
 });
-
-destroy.addEventListener('click', () => {
+const clearData = () => {
   boxes.innerHTML = '';
   inputEl.value = '';
-});
+};
+destroy.addEventListener('click', clearData);
